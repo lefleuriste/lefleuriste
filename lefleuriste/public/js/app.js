af4ca0e5eb53d3e6c1f3e3223212e1e134ac884f
mathieu <mathieu.piedanna@outlook.com>
2013-02-07 21:20:01 +0100

    Suppression d'une fonction dans app.js

--- lefleuriste/public/js/app.js
+++ lefleuriste/public/js/app.js
@@ -1,33 +1,6 @@
 (function($){ 
   //gestion des sous catégories pour la fonction modifier
   $('.ajaxList').live('change',function(event){
-	  
-         event.preventDefault();
-   		 var select = $(this);
-         var url = select.data('url')+select.val();		 
-         var id = '#'+select.data('target');
-         $.get(url,{},function(data){
-            var target = $(id).get(0);               
-            if(data.error){
-				
-              $(id).parents('.control-group').hide(); 
-              target.options.length = 0;
-            }else{
-			  console.log(data);	
-              $(id).parents('.control-group').show();                             
-              for(var i in data.results){   
-			  
-			                
-                var result = data.results[i];
-                target.options[i] = new Option(result.nom, result.id, false, false);
-              }
-            }
-            
-         },'json'); 		
-   }); 
-
-  //gestion des sous catégories pour la fonction modifier
-  $('.ajaxList').live('change',function(event){
     
          event.preventDefault();
        var select = $(this);
