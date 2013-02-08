f09da8ad0c519184506325928499e7e15312a757
Pedro <ppedro.sousa.k15@hotmail>
2013-02-08 16:34:37 +0100

    Changement du menu admin

--- lefleuriste/application/views/elements/menuadmin.blade.php
+++ lefleuriste/application/views/elements/menuadmin.blade.php
@@ -1,12 +1,12 @@
 <ul class="shortcut-buttons-set">
         
         <li><a class="shortcut-button" href="{{URL::to_action('categories.categories')}}"><span>
-          <img src="" alt="icon" /><br />
+          {{HTML::image('public/img/icon_categories.png')}}<br />
           Gestion des catégories
         </span></a></li>
         
         <li><a class="shortcut-button" href="{{URL::to_action('products.products')}}"><span>
-          <img src="" alt="icon" /><br />
+          {{HTML::image('public/img/icon_produits.png')}}<br />
           Gestion des produits
         </span></a></li>  
         
