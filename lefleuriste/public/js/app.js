87c540f07476c90656429715bcf17ec92e600cbc
mathieu <mathieu.piedanna@outlook.com>
2013-02-08 13:36:37 +0100

    Correction d'un bug des sélection des sous catégories

--- lefleuriste/public/js/app.js
+++ lefleuriste/public/js/app.js
@@ -8,18 +8,15 @@
          var id = '#'+select.data('target');
          $.get(url,{},function(data){
             var target = $(id).get(0);               
-            if(data.error){
-        
+            if(data.error){        
               $(id).parents('.control-group').hide(); 
               target.options.length = 0;
             }else{
-        console.log(data);  
+         
               $(id).parents('.control-group').show();                             
-              for(var i in data.results){   
-        
-                      
-                var result = data.results[i];
-                target.options[i] = new Option(result.nom, result.id, false, false);
+              for(var i in data.results){                     
+                var result = data.results[i];              
+                target.options[i] = new Option(result.nomc, result.id, false, false);
               }
             }
             
