cb1de56b97f4fcd310f4b3a6e8931b9e417c318b
mathieu <mathieu.piedanna@outlook.com>
2013-02-11 11:43:55 +0100

    Modification du menu dans l'administration

--- lefleuriste/application/views/elements/menuadmin.blade.php
+++ lefleuriste/application/views/elements/menuadmin.blade.php
@@ -2,6 +2,11 @@
         
         <li><a class="shortcut-button" href="{{URL::to_action('categories.categories')}}"><span>
           {{HTML::image('public/img/icon_categories.png')}}<br />
+          Gestion du compte
+        </span></a></li>
+
+        <li><a class="shortcut-button" href="{{URL::to_action('categories.categories')}}"><span>
+          {{HTML::image('public/img/icon_categories.png')}}<br />
           Gestion des catégories
         </span></a></li>
         
