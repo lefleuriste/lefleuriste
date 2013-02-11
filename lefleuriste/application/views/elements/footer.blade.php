0385e3fef33021e3bef6f91c38e0a68023f5a49e
Pedro <ppedro.sousa.k15@hotmail>
2013-02-11 14:59:00 +0100

    traitement du footer

--- lefleuriste/application/views/elements/footer.blade.php
+++ lefleuriste/application/views/elements/footer.blade.php
@@ -1,40 +1,10 @@
 <div id="footer">
 		 <div class="container">
-		    <p class="muted_credit">
+		    <div class="muted_credit">
             
-		    <!-- SARL JUCHAU, 147 Avenue des Arenes Romaines 31300 Toulouse - 05 61 31 77 77<br/>-->
-		    &copy; Copyright Association Junior Miage Bordeaux 2013 <br/>
-                            www.jumbo.miagebordeaux.fr - juniormiagebordeaux@gmail.com
-                            <p/>
-                     
-        <div id="links_footer_wrapper">
-            <div id="links_footer1" class="links_footer">
-                <ul>
-                    <li><a href="/">Accueil</a></li>
-                    <li><a href="index2.php?lang=fr&section=faqs">F.A.Q.</a></li>
-                    <li><a href="index2.php?lang=fr&section=service_clientele">Service &agrave;&nbsp; la client&egrave;le</a></li>
-                    <li><a href="index2.php?lang=fr&section=contacts">Contact</a></li>
-                    <li><a href="index2.php?lang=fr&section=about_us">&Agrave; propos de nous</a></li>
-                </ul>
+		    	<!-- SARL JUCHAU, 147 Avenue des Arenes Romaines 31300 Toulouse - 05 61 31 77 77<br/>-->
+		    	&copy; Copyright Association Junior Miage Bordeaux 2013 <br/>
+                www.jumbo.miagebordeaux.fr - juniormiagebordeaux@gmail.com
             </div>
-            <div id="links_footer2" class="links_footer">
-                <ul>                            
-                    <li><a href="http://www.webfloristsnetwork.com/index.php?cy=canada&lang=fr&section=livraisons_internationales" target="_blank">Livraison internationale</a></li>
-                    <li><a href="index2.php?lang=fr&section=security">S&eacute;curit&eacute; et Confidentialit&eacute;</a></li>
-                    <li><a href="plants/">Entretien des plantes</a></li>
-                    <li><a href="index2.php?lang=fr&section=entretien_fleurs">Entretien des fleurs</a></li>
-                    <li><a href="index2.php?lang=fr&section=definition_fleurs">D&eacute;finition des fleurs</a></li>
-                </ul>
-            </div>
-            <div id="links_footer3" class="links_footer">
-                <ul>
-                    <li><a href="index2.php?lang=fr&section=wallpaper">Fond d'&eacute;cran</a></li>
-                    <li><a href="index2.php?lang=fr&section=garantie">Garantie</a></li>
-                    <li><a href="index2.php?lang=fr&section=liens_interessant">Liens int&eacute;ressants</a></li>                            
-                    <li><a href="index2.php?lang=fr&section=sitemap">Sitemap</a></li>
-                </ul>
-            </div>
-            
-        </div>
     </div>
 </div>
